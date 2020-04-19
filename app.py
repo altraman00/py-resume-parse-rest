a9be69c6922d9113c65862087e0ffef3ce032ccf
谢昆 <xiekun@sunlands.com>
2020-04-19 17:29:36 +0800

    添加post和get带参测试接口 # POST方法访问 # 入参在postman中采用 Body -> raw -> JSON(application/json)
    # GET方法访问 入参在url后添加
    # 请求方式 http://127.0.0.1:5000/test_1.0?name=张三&age=18

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ import html_parse
 app = Flask(__name__)
 
 
+# hello world接口
 @app.route('/hello', methods=['GET'])
 def hello_world():
     return 'hello world!'
 
 
+# 解析简历的接口
 @app.route('/resume/parse', methods=['GET'])
 def resume_parse():
     url = "https://baike.baidu.com/item/Python/407313"
@@ -32,9 +34,10 @@ def resume_parse():
     return res_data_summary
 
 
-# 只接受get方法访问
+# 只接受get方法访问 入参在url后添加
+# 请求方式 http://127.0.0.1:5000/test_1.0?name=张三&age=18
 @app.route("/test_1.0", methods=["GET"])
-def check():
+def check1():
     # 默认返回内容
     return_dict = {'return_code': '200', 'return_info': '处理成功', 'result': False}
     # 判断入参是否为空
@@ -58,6 +61,27 @@ def tt(name, age):
     return result_str
 
 
+# 只接受POST方法访问
+# 入参在postman中采用 Body -> raw -> JSON(application/json)
+@app.route("/test_1.1", methods=["POST"])
+def check2():
+    # 默认返回内容
+    return_dict = {'return_code': '200', 'return_info': '处理成功', 'result': False}
+    # 判断传入的json数据是否为空
+    if request.get_data() is None:
+        return_dict['return_code'] = '5004'
+        return_dict['return_info'] = '请求参数为空'
+        return json.dumps(return_dict, ensure_ascii=False)
+    # 获取传入的参数
+    get_data = request.get_data()
+    # 传入的参数为bytes类型，需要转化成json
+    get_data = json.loads(get_data)
+    name = get_data.get('name')
+    age = get_data.get('age')
+    # 对参数进行操作
+    return_dict['result'] = tt(name, age)
+
+    return json.dumps(return_dict, ensure_ascii=False)
 
 
 if __name__ == '__main__':
