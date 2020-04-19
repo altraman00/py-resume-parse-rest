f1cd3edc1ee875182ab133ec77291f59b095a941
谢昆 <xiekun@sunlands.com>
2020-04-19 17:15:13 +0800

    添加测试接口

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import urllib
 from _curses import error
 
 from bs4 import BeautifulSoup
-from flask import Flask
+from flask import Flask, request, json
 
 import html_parse
 
@@ -32,8 +32,33 @@ def resume_parse():
     return res_data_summary
 
 
+# 只接受get方法访问
+@app.route("/test_1.0", methods=["GET"])
+def check():
+    # 默认返回内容
+    return_dict = {'return_code': '200', 'return_info': '处理成功', 'result': False}
+    # 判断入参是否为空
+    if request.args is None:
+        return_dict['return_code'] = '5004'
+        return_dict['return_info'] = '请求参数为空'
+        return json.dumps(return_dict, ensure_ascii=False)
+    # 获取传入的params参数
+    get_data = request.args.to_dict()
+    name = get_data.get('name')
+    age = get_data.get('age')
+    # 对参数进行操作
+    return_dict['result'] = tt(name, age)
+
+    return json.dumps(return_dict, ensure_ascii=False)
+
+
+# 功能函数
+def tt(name, age):
+    result_str = "%s今年%s岁" % (name, age)
+    return result_str
+
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
