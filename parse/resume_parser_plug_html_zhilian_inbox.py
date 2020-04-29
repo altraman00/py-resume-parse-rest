2d6cb559368e533aa2c07b0a4cc75f6f97865005
谢昆 <xiekun@sunlands.com>
2020-04-29 16:43:38 +0800

    修改命名

--- parse/resume_parser_plug_html_zhilian_inbox.py
+++ parse/resume_parser_plug_html_zhilian_inbox.py
@@ -11,14 +11,14 @@ from bs4 import BeautifulSoup
 class ResumeParserPlugHtmlZhilianInbox(object):
 
     # 读取html文件
-    def readFile(self, htmlPath):
+    def func_read_file(self, htmlPath):
         html = open(htmlPath, mode='r', encoding='utf-8').read()
         return html
 
     # 将html简历按照板块区分
-    def getResumeModuleDict(self, htmlStr):
+    def func_get_resume_module_dict(self, htmlStr):
         moduleDict = {}
-        soup = self.soupHtml(htmlStr)
+        soup = self.func_soup_html(htmlStr)
 
         # ------------------------------------------------基本信息------------------------------------------------
         基本信息_element = soup.find(class_="resume-content__candidate-basic")
@@ -80,10 +80,10 @@ class ResumeParserPlugHtmlZhilianInbox(object):
 
         return moduleDict
 
-    def parseResumeHtmlModule(self, moduleDict):
+    def func_parse_resume_html_module(self, moduleDict):
         print('\n------------------------------------------------基本信息------------------------------------------------')
         基本信息 = moduleDict['基本信息']
-        self.getSeekerBaseInfo(基本信息)
+        self.func_get_seeker_base_info(基本信息)
 
         print('\n------------------------------------------------求职意向------------------------------------------------')
         求职意向 = moduleDict['求职意向']
@@ -91,35 +91,35 @@ class ResumeParserPlugHtmlZhilianInbox(object):
 
         print('\n------------------------------------------------教育经历------------------------------------------------')
         教育经历 = moduleDict['教育经历']
-        self.getEduBaseInfo(教育经历)
+        self.func_get_edu_baseinfo(教育经历)
 
         print('\n------------------------------------------------工作经验------------------------------------------------')
         工作经验 = moduleDict['工作经验']
-        self.getWorkExpBaseInfo(工作经验)
+        self.func_get_work_exp_baseinfo(工作经验)
 
         print('\n------------------------------------------------项目经验------------------------------------------------')
         项目经验 = moduleDict['项目经验']
-        self.getProjectExpBaseInfo(项目经验)
+        self.func_get_project_exp_baseInfo(项目经验)
 
         print('\n------------------------------------------------专业技能------------------------------------------------')
         专业技能 = moduleDict['专业技能']
-        self.getSkillBaseInfo(专业技能)
+        self.func_get_skill_baseInfo(专业技能)
 
         print('\n------------------------------------------------自我评价------------------------------------------------')
         自我评价 = moduleDict['自我评价']
-        self.getSelfEvalBaseInfo(自我评价)
+        self.func_get_self_eval_baseInfo(自我评价)
 
         print('\n------------------------------------------------培训经历------------------------------------------------')
         培训经历 = moduleDict['培训经历']
-        self.getTrainBaseInfo(培训经历)
+        self.func_get_train_baseinfo(培训经历)
 
     # 用BeautifulSoup亲吻一遍html
-    def soupHtml(self, baseText):
+    def func_soup_html(self, baseText):
         soup = BeautifulSoup(baseText, 'html.parser', from_encoding='utf-8')
         return soup
 
-    def getSeekerBaseInfo(self, 基本信息):
-        soup = self.soupHtml(基本信息)
+    def func_get_seeker_base_info(self, 基本信息):
+        soup = self.func_soup_html(基本信息)
 
         姓名 = soup.find('span', attrs={'data-bind': 'textQ: candidateName'}).text
         性别 = soup.find('span', attrs={'data-bind': '$key: genderDesc'}).text
@@ -134,8 +134,8 @@ class ResumeParserPlugHtmlZhilianInbox(object):
         print("姓名:%s\n性别:%s\n年龄:%s\n工作年限:%s\n学历:%s\n当前城市:%s\n户口:%s\n手机:%s\n邮箱:%s"
               % (姓名, 性别, 年龄, 工作年限, 学历, 当前城市, 户口, 手机, 邮箱))
 
-    def getjobhuntbaseinfo(self, 求职意向):
-        soup = self.soupHtml(求职意向)
+    def func_get_jobhunt_baseinfo(self, 求职意向):
+        soup = self.func_soup_html(求职意向)
         keys = soup.find('dl', attrs={'data-bind': "foreach: { data: intents, as: 'intent' }"}).find_all('dt')
         values = soup.find('dl', attrs={'data-bind': "foreach: { data: intents, as: 'intent' }"}).find_all('dd')
         jobHuntDict = {}
@@ -147,31 +147,31 @@ class ResumeParserPlugHtmlZhilianInbox(object):
         print('jobHuntDict',jobHuntDict)
 
 
-    def getTrainBaseInfo(self, 培训经历):
+    def func_get_train_baseinfo(self, 培训经历):
         pass
 
-    def getSelfEvalBaseInfo(self, 自我评价):
+    def func_get_self_eval_baseInfo(self, 自我评价):
         pass
 
-    def getSkillBaseInfo(self, 专业技能):
+    def func_get_skill_baseInfo(self, 专业技能):
         pass
 
-    def getProjectExpBaseInfo(self, 项目经验):
+    def func_get_project_exp_baseInfo(self, 项目经验):
         pass
 
-    def getWorkExpBaseInfo(self, 工作经验):
+    def func_get_work_exp_baseinfo(self, 工作经验):
         '''
         :param 工作经验:
         :return:
         '''
 
-    def getEduBaseInfo(self, 教育经历):
+    def func_get_edu_baseinfo(self, 教育经历):
             pass
 
 
 if __name__ == '__main__':
     zhilian = ResumeParserPlugHtmlZhilianInbox()
     htmlPath = '/Users/admin/Desktop/简历解析/智联招聘/智联招聘-插件-已下载模块-openFrom-2-李宁-直接报错.html'
-    htmlStr = zhilian.readFile(htmlPath)
-    moduleDict = zhilian.getResumeModuleDict(htmlStr)
-    zhilian.parseResumeHtmlModule(moduleDict)
+    htmlStr = zhilian.func_read_file(htmlPath)
+    moduleDict = zhilian.func_get_resume_module_dict(htmlStr)
+    zhilian.func_parse_resume_html_module(moduleDict)
